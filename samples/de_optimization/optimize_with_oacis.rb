c586ea24bfae23236b02a5469a2d259bea328ef3
Yohsuke Murase <yohsuke.murase@gmail.com>
2016-10-21 16:44:34 +0900

    implemented OACIS integration

--- samples/de_optimization/optimize_with_oacis.rb
+++ samples/de_optimization/optimize_with_oacis.rb
@@ -1,31 +1,31 @@
 require_relative "de_optimizer"
 
-opt = DE_Optimizer.new( n: 10, f: 0.5, cr: 0.2 )
-domains = [    # 3-dimensional input variables. Rounded by one digits.
-  {min: -10.0, max: 10.0, round: 2},
-  {min: -10.0, max: 10.0, round: 2},
-  {min: -10.0, max: 10.0, round: 2}
+logger = Logger.new($stderr)
+
+domains = [
+  {min: -10.0, max: 10.0, eps: Rational(1,10)},
+  {min: -10.0, max: 10.0, eps: Rational(1,10)},
+  {min: -10.0, max: 10.0, eps: Rational(1,10)}
 ]
-opt.domains = domains
 
 sim = Simulator.where(name: "de_optimize_test").first
 host = Host.where(name:"localhost").first
 
-logger = Logger.new($stderr)
-
-opt.calc_f = lambda do |points|
-  pss = points.map do |point|
-    ps = sim.find_or_create_parameter_set( p1:point[0], p2:point[1], p3: point[2] )
-    ps.find_or_create_runs_upto(1, submitted_to: host, host_param: host.default_parameters)
+map_agents = lambda {|agents|
+  parameter_sets = agents.map do |x|
+    ps = sim.find_or_create_parameter_set( p1:x[0], p2:x[1], p3: x[2] )
+    ps.find_or_create_runs_upto(1, submitted_to: host, host_param: host.default_host_parameters)
     logger.info "Created a new PS: #{ps.v}"
     ps
   end
-  OacisWatcher::start(logger: logger) {|w| w.watch_all_ps( pss ) {} }
-  pss.map {|ps| ps.reload.runs.first.result["f"] }
-end
+  OacisWatcher::start( logger: logger ) {|w| w.watch_all_ps( parameter_sets ) {} }
+  parameter_sets.map {|ps| ps.runs.first.result["f"] }
+}
 
-3.times do |t|
+opt = DE_Optimizer.new(map_agents, domains, n: 30, f: 0.8, cr: 0.9, rand_seed: 1234)
+
+20.times do |t|
   opt.proceed
-  puts "#{opt.t} #{opt.best_point} #{opt.best_val}"
+  puts "#{opt.t} #{opt.best_point} #{opt.best_f} #{opt.average_f}"
 end
 
